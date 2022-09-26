07d8a38459ea19e16a31458d8725603723ba194b
niteshsingh29 <nksingh.2907@gmail.com>
2022-09-26 23:45:10 +0530

    dsa in c++

---
+++ index.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////////// C++ codechef leetcode contest solutions ////////////////////////////////////////
+
+// 1
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int,string>>v;
+    int n = names.size();
+    
+    for (int i=0; i<n; i++){
+        v.push_back({heights[i],names[i]});
+    }
+    
+    sort(v.rbegin(),v.rend());
+    
+    for(int i=0; i<n; i++) {
+        names[i] = v[i].second;
+    }
+    
+    return names;
+
+    }
+};
