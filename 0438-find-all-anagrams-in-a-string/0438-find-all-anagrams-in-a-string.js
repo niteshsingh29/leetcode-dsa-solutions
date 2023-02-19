7e33af817142ed933277437201e851067964156e
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2023-02-19 10:01:11 +0530

    Time: 401 ms (21.68%), Space: 49.4 MB (38.47%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    let sHash = new Array(26).fill(0);
+    let pHash = new Array(26).fill(0);
+    
+    let length = p.length;
+    let res = new Array();
+
+    if(p.length > s.length) return res;
+
+    for(const char of p) {
+        pHash[char.charCodeAt(0) - 97]++;
+    }
+
+    while(length--) {
+        sHash[s[length].charCodeAt(0) - 97]++;
+    }
+
+    for(let i=0; i<=s.length-p.length; i++) {
+        if (JSON.stringify(pHash) === JSON.stringify(sHash)) {
+            res.push(i)
+        }
+        sHash[s[i].charCodeAt(0) - 97]--;
+        sHash[s[i+(p.length)]?.charCodeAt(0) - 97]++;
+    }
+
+    return res;
+
+};
