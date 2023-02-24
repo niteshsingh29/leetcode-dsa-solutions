f330fedf0f014cb71b63ac32c06fc1f3b2925eb7
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2023-02-24 22:53:59 +0530

    Time: 5664 ms (50.00%), Space: 65.5 MB (100.00%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeviation = function(nums) {
+    
+    nums.forEach( (n,i) => n%2 && ( nums[i] *= 2 ) )
+    nums = [ ...new Set(nums) ].sort( (n,m) => m-n )
+    
+    let l = nums.length-1, dev = nums[0]-nums[l], i
+    
+    while ( nums[0]%2 == 0 ) {
+        if ( (nums[0] /= 2) < nums[1] )
+            i = nums.findIndex( n => n < nums[0] ),
+            nums.splice( i<0 ? l : i-1 , 0, nums.shift() )
+        dev = Math.min( dev, nums[0]-nums[l] )
+    }
+    return dev
+};
