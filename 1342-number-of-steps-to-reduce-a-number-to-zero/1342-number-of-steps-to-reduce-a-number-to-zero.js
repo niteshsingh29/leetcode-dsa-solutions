ed3ea344b8f5add7954fb22a364d1e1c1156e465
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2023-03-19 03:26:07 +0530

    Time: 60 ms (74.07%), Space: 42 MB (45.04%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    var steps = 0;
+    
+    while(num!=0) {
+        if(num%2==0) {
+            num = num / 2;
+        } else {
+            num = num - 1;
+        }
+        steps++;
+    }
+    
+    return steps;
+};
