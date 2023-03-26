b2470653548a89413087d36b15107a614dc0db12
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2023-03-26 22:09:45 +0530

    Time: 114 ms (38.48%), Space: 42.3 MB (68.41%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    let arr = [];
+    
+    for(let i=0; i<nums.length; i++) {
+        for (let j=i+1; j<nums.length; j++) {
+            if((nums[i] + nums[j]) == target) {
+                arr = [i,j];
+                return arr;
+            }
+        }
+    }
+    
+    
+};
