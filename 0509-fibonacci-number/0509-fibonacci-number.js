a50d0f86274ddbbbed130d411c8291305053fc70
Nitesh Singh <92999235+niteshsingh29@users.noreply.github.com>
2023-06-08 05:16:09 +0530

    Time: 57 ms (76.54%), Space: 41.4 MB (91.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,14 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-    // dynamic programming with memoization; top-down approach
-    let dp = new Array(n).fill(-1);
-    
-    if(n==0) return 0;
-    if(n==1) return 1;
-    
-    if(dp[n]) return dp[n];
-    
-    return dp[n] = fib(n-1) + fib(n-2);
+// dynamic programming with memoization; top-down approach
+var dp = new Array(31).fill(-1);
+var fib = function (n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+
+  if (dp[n] !== -1) return dp[n];
+
+  return (dp[n] = fib(n - 1) + fib(n - 2));
 };
